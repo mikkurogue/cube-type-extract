5b1d4470ca83762fe22ab7045daf46dc528de609
mikkurogue <michael.lindemans@outlook.com>
2024-08-26 16:54:10 +0200

    add better logging. also make it that it generated an all type of the unions incase we need to be able to see all dimension names regardless of cube

--- main.go
+++ main.go
@@ -18,37 +18,32 @@ var cubeNames string
 
 func main() {
 
-	file := flag.String("file", "cubejs-types", "Give a file name without an extension")
 	rename := flag.Bool("rename", false, "Rename the file type prefixes")
+	fileName := flag.String("filename", "cubejs-types", "Rename the file without a file extension")
 
 	flag.Parse()
 
 	var generator gen.Generator
 
-	generator.FileName = *file
+	generator.FileName = *fileName
 
 	generator.FetchMetadata()
 
 	// check if the flag is set to true, then we start a form to rename the cube metadata
 	if *rename == true {
 		// map over the generator cube count and assign custom prefixes
-		generateInputs(generator)
+		for i := 0; i < generator.CubeCount; i++ {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Printf("Enter a prefix for the cube '%v': ", generator.Metadata.Cubes[i].Name)
+			text, _ := reader.ReadString('\n')
+			text = strings.TrimSuffix(text, "\n")
+
+			generator.Metadata.Cubes[i].Name = text
+		}
 	}
 
 	generator.IterateToGenerate()
 
 	// Kill the app when complete.
 	os.Exit(0)
-}
-
-func generateInputs(generator gen.Generator) {
-	for i := 0; i < generator.CubeCount; i++ {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Printf("Enter a prefix for the cube '%v': ", generator.Metadata.Cubes[i].Name)
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSuffix(text, "\n")
-
-		generator.Metadata.Cubes[i].Name = text
-	}
-
 }
