ac8dab977f557f3712f3fe2b7ee57b74aeb0ea8c
mikkurogue <michael.lindemans@outlook.com>
2024-08-26 15:57:43 +0200

    abstract a function - i want to get charmbracelet working here properly with generated inputs...

--- main.go
+++ main.go
@@ -32,18 +32,23 @@ func main() {
 	// check if the flag is set to true, then we start a form to rename the cube metadata
 	if *rename == true {
 		// map over the generator cube count and assign custom prefixes
-		for i := 0; i < generator.CubeCount; i++ {
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Printf("Enter a prefix for the cube '%v': ", generator.Metadata.Cubes[i].Name)
-			text, _ := reader.ReadString('\n')
-			text = strings.TrimSuffix(text, "\n")
-
-			generator.Metadata.Cubes[i].Name = text
-		}
+		generateInputs(generator)
 	}
 
 	generator.IterateToGenerate()
 
 	// Kill the app when complete.
 	os.Exit(0)
+}
+
+func generateInputs(generator gen.Generator) {
+	for i := 0; i < generator.CubeCount; i++ {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Enter a prefix for the cube '%v': ", generator.Metadata.Cubes[i].Name)
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+
+		generator.Metadata.Cubes[i].Name = text
+	}
+
 }
